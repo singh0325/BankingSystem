3cf8d1d754f9297305b2241a88989924a938a46f
singh0325 <satyam.singh@stud.vilniustech.lt>
2026-05-09 21:48:15 +0300

    comments

--- SavingsAccount.cpp
+++ SavingsAccount.cpp
@@ -1,12 +1,15 @@
 #include "SavingsAccount.h"
 
+// Constructs savings account with base account details and interest rate
 SavingsAccount::SavingsAccount(const std::string& name, double balance, double rate)
     : Account(name, balance), interestRate(rate) {}
 
+// Adds interest earnings to current balance based on stored interest rate
 void SavingsAccount::applyInterest() {
     balance += balance * interestRate;
 }
 
+// Overrides display to label account as Savings before showing base details
 void SavingsAccount::display() const {
     std::cout << "[Savings] ";
     Account::display();
